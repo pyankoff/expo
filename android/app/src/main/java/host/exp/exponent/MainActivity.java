a70f5a6ed41adeeb8ecf2aa90e098ae62fe08bf7
Stanisław Chmiela <sjchmiela@gmail.com>
2018-11-27 09:12:12 +0100

    Remove ShellAppActivity, let's always use MainActivity

--- android/app/src/main/java/host/exp/exponent/MainActivity.java
+++ android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactPackage;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import expo.core.interfaces.Package;
@@ -47,11 +45,6 @@ public class MainActivity extends DetachActivity {
   }
 
   @Override
-  public List<String> sdkVersions() {
-    return new ArrayList<>(Arrays.asList("31.0.0"));
-  }
-
-  @Override
   public List<ReactPackage> reactPackages() {
     return ((MainApplication) getApplication()).getPackages();
   }
@@ -60,6 +53,7 @@ public class MainActivity extends DetachActivity {
   public List<Package> expoPackages() {
     return Arrays.<Package>asList(
         new CameraPackage(),
+        new ConstantsPackage(),
         new SensorsPackage(),
         new FileSystemPackage(),
         new FaceDetectorPackage(),
